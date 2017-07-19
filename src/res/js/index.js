a1dc4c5ff1fc9d9cdb1da40164c07d685957fed2
黄剑 <huangjian@guoganpai.com>
2017-07-19 17:17:43 +0800

    高度修正

--- src/res/js/index.js
+++ src/res/js/index.js
@@ -4,16 +4,23 @@
     $(function () {
 
         /*高度*/
- /*       var realHeight  = $('body').height();
-        var scale = realHeight/1080;
-        console.log(scale);
+        function getScale() {
+            var fontSize = $('html').css('fontSize');
+            var scale = 1;
+            fontSize = ~fontSize.indexOf('px')?fontSize.slice(0,-2):fontSize;
+            if (fontSize !== undefined) {
+                var realHeight = $('body').height();
+                scale = realHeight/(10.8*fontSize);
+            }
+            console.log('scale:'+scale);
+            console.log('realHeight:'+realHeight);
+            return scale;
+        }
 
-        $('body').css({transform:'scaleY('+scale+')'})
-        $(window).on('resize',function () {
-            var realHeight  = $('body').height();
-            var scale = realHeight/1080;
-            $('body').css({transform:'scaleY('+scale+')'})
-        })*/
+
+
+        $('.mywp-wrap').css({transform: 'scaleY(' + getScale() + ')',transformOrigin:'left top'});
+
         /*子菜单收缩呈现*/
         function subToggleShow() {
             var show = true;
@@ -93,42 +100,45 @@
         function Dot(list) {
             creatInfDot(list)
         }
+
         function creatInfDot(list) {
-            for(var i =0,ii=list.length;i<ii;i++){
+            for (var i = 0, ii = list.length; i < ii; i++) {
                 $('.cfxtImg').append(createDot(list[i]));
             }
         }
+
         // 创建信息点
         function createDot(dot) {
-            if(dot&&dot.x&&dot.y){
+            if (dot && dot.x && dot.y) {
                 var cssObj = {
-                    position:'absolute',
-                    left:dot.x+'rem',
-                    top:dot.y*.89+'rem'
+                    position: 'absolute',
+                    left: dot.x + 'rem',
+                    top: dot.y * .89 + 'rem'
                 };
-                var handleDot =  $('<span class="dot"></span>').css(cssObj);
+                var handleDot = $('<span class="dot"></span>').css(cssObj);
                 //绑定提示框
                 handleDot.click(function () {
                     infToast(dot);
                 });
                 return handleDot;
-            }else{
+            } else {
                 console.log('x,y是必要参数');
             }
         }
+
         // 提示框
         function infToast(dot) {
             // 清除已存在的提示
             var toast = $('.toast');
-            toast&&toast.remove();
+            toast && toast.remove();
 
             var cssObj = {
-                position:'absolute',
-                left:dot.x+'rem',
-                top:dot.y*.89+'rem',
-                width:'100px',
-                height:'100px',
-                backgroundColor:'#fff'
+                position: 'absolute',
+                left: dot.x + 'rem',
+                top: dot.y * .89 + 'rem',
+                width: '100px',
+                height: '100px',
+                backgroundColor: '#fff'
             };
             var inftoast = $('<div class="toast">123123</div>').html(dot.inf).css(cssObj);
             $('.cfxtImg').append(inftoast);
@@ -136,7 +146,7 @@
 
         // 边界监测
         function borderCheck() {
-            
+
         }
 
     })

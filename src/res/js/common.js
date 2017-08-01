d723f3cedec6c736beaf45158c154e7dcfd45441
huangjian <106025154@qq.com>
2017-08-01 23:45:59 +0800

    厂房

--- src/res/js/common.js
+++ src/res/js/common.js
@@ -3,6 +3,8 @@
     var mywp = {
         setAdaption:setAdaption,
         drawSvg:drawSvg,
+        fullScreen:fullScreen,
+        dot:dot
     };
 
 
@@ -28,13 +30,80 @@
         refresh();
     }
 
+    //屏幕高
+    function fullScreen() {
+        function getScale() {
+            var fontSize = $('html').css('fontSize');
+            var scale = 1;
+            fontSize = ~fontSize.indexOf('px') ? fontSize.slice(0, -2) : fontSize;
+            if (fontSize !== undefined) {
+                var realHeight = $('body').height();
+                scale = realHeight / (10.8 * fontSize);
+            }
+            return scale;
+        };
+        $(window).on('resize', function () {
+            $('.mywp-wrap').css({transform: 'scaleY(' + getScale() + ')'});
+        });
+        $('.mywp-wrap').css({transform: 'scaleY(' + getScale() + ')'});
+    }
+
+    // 绘制SVG
     function drawSvg(id,pathData,callback) {
         d3.select(id).selectAll(".build").data(pathData).enter().append("path").attr("class","build").attr("d",function (data) {
             return data.d;
         }).style({"fill":"transparent","cursor":"pointer"}).on("mouseover",callback.mouseOver).on("mouseout",callback.mouseOut);
+        d3.select(".mywp-cf").style("overflow","hidden");
     }
 
-    //屏幕高
+    // 信息点
+    function dot(list) {
+        creatInfDot(list)
+        function creatInfDot(list) {
+            for (var i = 0, ii = list.length; i < ii; i++) {
+                $('.cfxtImg').append(createDot(list[i]));
+            }
+        }
+        // 创建信息点
+        function createDot(dot) {
+            if (dot && dot.x && dot.y) {
+                var cssObj = {
+                    position: 'absolute',
+                    left: dot.x + 'rem',
+                    top: dot.y * .89 + 'rem'
+                };
+                var handleDot = $('<span class="dot"></span>').css(cssObj);
+                //绑定提示框
+                handleDot.click(function () {
+                    infToast(dot);
+                });
+                return handleDot;
+            } else {
+                console.log('x,y是必要参数');
+            }
+        }
+        // 提示框
+        function infToast(dot) {
+            // 清除已存在的提示
+            var toast = $('.toast');
+            toast && toast.remove();
+
+            var cssObj = {
+                position: 'absolute',
+                left: dot.x + 'rem',
+                top: dot.y * .89 + 'rem',
+                width: '100px',
+                height: '100px',
+                backgroundColor: '#fff'
+            };
+            var inftoast = $('<div class="toast">123123</div>').html(dot.inf).css(cssObj);
+            $('.cfxtImg').append(inftoast);
+        }
+        // 边界监测
+        function borderCheck() {
+        };
+    }
+
 
 
     if (typeof define === "function" && define.amd) {
--- src/res/js/index.js
+++ src/res/js/index.js
@@ -1,23 +1,11 @@
 (function ($, mywp, d3) {
     mywp.setAdaption(19.2);
 
+
     $(function () {
 
         /*高度*/
-        function getScale() {
-            var fontSize = $('html').css('fontSize');
-            var scale = 1;
-            fontSize = ~fontSize.indexOf('px') ? fontSize.slice(0, -2) : fontSize;
-            if (fontSize !== undefined) {
-                var realHeight = $('body').height();
-                scale = realHeight / (10.8 * fontSize);
-            }
-            return scale;
-        };
-        $(window).on('resize', function () {
-            $('.mywp-wrap').css({transform: 'scaleY(' + getScale() + ')'});
-        });
-        $('.mywp-wrap').css({transform: 'scaleY(' + getScale() + ')'});
+        mywp.fullScreen();
 
         /*子菜单收缩呈现*/
         function subToggleShow() {
@@ -31,7 +19,6 @@
                 show = !show;
             }
         }
-
         $('#subToggle').on('click', subToggleShow());
 
         /*信息点生成*/
@@ -92,61 +79,10 @@
                 inf: '20*20'
             }
         ];
+        mywp.dot(dots);
 
-        Dot(dots);
-
-        function Dot(list) {
-            creatInfDot(list)
-        }
-
-        function creatInfDot(list) {
-            for (var i = 0, ii = list.length; i < ii; i++) {
-                $('.cfxtImg').append(createDot(list[i]));
-            }
-        }
-
-        // 创建信息点
-        function createDot(dot) {
-            if (dot && dot.x && dot.y) {
-                var cssObj = {
-                    position: 'absolute',
-                    left: dot.x + 'rem',
-                    top: dot.y * .89 + 'rem'
-                };
-                var handleDot = $('<span class="dot"></span>').css(cssObj);
-                //绑定提示框
-                handleDot.click(function () {
-                    infToast(dot);
-                });
-                return handleDot;
-            } else {
-                console.log('x,y是必要参数');
-            }
-        }
-
-        // 提示框
-        function infToast(dot) {
-            // 清除已存在的提示
-            var toast = $('.toast');
-            toast && toast.remove();
-
-            var cssObj = {
-                position: 'absolute',
-                left: dot.x + 'rem',
-                top: dot.y * .89 + 'rem',
-                width: '100px',
-                height: '100px',
-                backgroundColor: '#fff'
-            };
-            var inftoast = $('<div class="toast">123123</div>').html(dot.inf).css(cssObj);
-            $('.cfxtImg').append(inftoast);
-        }
-
-        // 边界监测
-        function borderCheck() {
-        };
-
-
+        /*厂房信息*/
+        // 路径
         var paths = [
             {id: '1', name: '1', d: "M639,310l112,65l233-131v-56l6-5l-56-46l-63-22L633,252v2l6,1V310z"},
             {
@@ -167,7 +103,7 @@
                 d: "M0,402l99,56c0,0,45.5,20.5,73,7l181-103l-4-4l-2-46l5-3l-145-74l-47,15l-41,35l-50,18l-44,33L0,349l4,2v46L0,402z"
             },
         ];
-
+        //建筑信息
         var buildInf = {
             1: {data: 1},
             2: {data: 2},
@@ -176,6 +112,7 @@
             5: {data: 5},
             6: {data: 6}
         };
+        //回调
         var callback ={
             'mouseOver':function (d) {
                 console.log(d);
@@ -186,6 +123,7 @@
                 $('.build'+d.id).hide();
             }
         }
+        // 绘制
         mywp.drawSvg("#build-inf", paths,callback);
 
 

aca855d462c7b30e5cb4d24a63a71a14fa9328d7
huangjian <106025154@qq.com>
2017-09-29 22:59:05 +0800

    9.13 UI时间

--- src/res/js/syntheyAnalysis.js
+++ src/res/js/syntheyAnalysis.js
@@ -30,7 +30,7 @@
                 x:'center',
                 textStyle:{
                     color:'#fff'
-                }
+                },
             },
             tooltip : {
                 trigger: 'item',
@@ -69,6 +69,113 @@
 
         // 使用刚指定的配置项和数据显示图表。
         myChart.setOption(option);
+
+
+        var myChart2 = echarts.init(document.getElementById('main2'));
+        console.log(myChart2);
+        console.log(myChart);
+        var option = {
+            title: {
+                text: '未来一周气温变化',
+                subtext: '纯属虚构',
+                textStyle:{
+                    color:'#fff'
+                }
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            legend: {
+                data:['最高气温','最低气温'],
+                textStyle:{
+                    color:'#fff'
+                },
+            },
+            toolbox: {
+                show: true,
+                feature: {
+                    dataZoom: {
+                        yAxisIndex: 'none'
+                    },
+                    dataView: {readOnly: false},
+                    magicType: {type: ['line', 'bar']},
+                    restore: {},
+                    saveAsImage: {}
+                }
+            },
+            xAxis:  {
+                type: 'category',
+                boundaryGap: false,
+                data: ['周一','周二','周三','周四','周五','周六','周日'],
+                nameTextStyle:{
+                    color:'#fff',
+                    fontSize:'24'
+                },
+                lineStyle:{
+                    color:'#fff'
+                }
+            },
+            yAxis: {
+                type: 'value',
+                axisLabel: {
+                    formatter: '{value} °C'
+                },
+                nameTextStyle:{
+                    color:'#fff'
+                }
+            },
+            series: [
+                {
+                    name:'最高气温',
+                    type:'line',
+                    data:[11, 11, 15, 13, 12, 13, 10],
+                    markPoint: {
+                        data: [
+                            {type: 'max', name: '最大值'},
+                            {type: 'min', name: '最小值'}
+                        ]
+                    },
+                    markLine: {
+                        data: [
+                            {type: 'average', name: '平均值'}
+                        ]
+                    }
+                },
+                {
+                    name:'最低气温',
+                    type:'line',
+                    data:[1, -2, 2, 5, 3, 2, 0],
+                    markPoint: {
+                        data: [
+                            {name: '周最低', value: -2, xAxis: 1, yAxis: -1.5}
+                        ]
+                    },
+                    markLine: {
+                        data: [
+                            {type: 'average', name: '平均值'},
+                            [{
+                                symbol: 'none',
+                                x: '90%',
+                                yAxis: 'max'
+                            }, {
+                                symbol: 'circle',
+                                label: {
+                                    normal: {
+                                        position: 'start',
+                                        formatter: '最大值'
+                                    }
+                                },
+                                type: 'max',
+                                name: '最高点'
+                            }]
+                        ]
+                    }
+                }
+            ]
+        };
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart2.setOption(option);
 
     })
 })(jQuery,mywp)
